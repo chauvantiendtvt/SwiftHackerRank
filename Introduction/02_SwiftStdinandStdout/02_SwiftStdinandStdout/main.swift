3706ca0263b8814fbe96fc95c69dd490c344aeed
ChauVanTien <chauvantiendtvt@gmail.com>
2017-07-11 07:47:42 +0700

    SwiftStdinandStdout

---
+++ Introduction/02_SwiftStdinandStdout/02_SwiftStdinandStdout/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  02_SwiftStdinandStdout
+//
+//  Created by Admin on 7/7/17.
+//  Copyright © 2017 ChauVanTien. All rights reserved.
+//
+
+import Foundation
+
+let string: String = "Swift"
+let int: Int = 2017
+print("Programming language \(string)")
+print("Year \(int)")
+
+
