a9c73d1f15808d62f914de7736c7540e903a6381
ChauVanTien <chauvantiendtvt@gmail.com>
2017-07-11 07:46:26 +0700

    Welcome to swift

---
+++ Introduction/01_WelcometoSwift/01_WelcometoSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  01_WelcometoSwift
+//
+//  Created by Admin on 7/7/17.
+//  Copyright © 2017 ChauVanTien. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Hello, Swift")
